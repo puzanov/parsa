a915be150170cc01dc0ed28d74b5cf154df627d7
puzanov <puzanov@gmail.com>
2011-03-02 12:10:43 +0600

    log getter test added

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "http://rubygems.org"
+gem "net-ssh", "2.1.2"
+gem "net-sftp", "2.0.5", :require => "net/sftp"
--- test/log_getter_test.rb
+++ test/log_getter_test.rb
@@ -1,11 +1,26 @@
+require "config"
 require "test/unit"
+require 'digest/md5'
 
 class LogGetterTest < Test::Unit::TestCase
+  @@remote_file = "/mnt/src/parsa/test/data/access.log.gz"
+  @@local_file  = "/mnt/src/parsa/test/data/from_sftp_access.log.gz"
+  
+  def setup
+    puts "deleting #{@@local_file}"
+    File.delete(@@local_file)
+  end
+  
   def test_get_file
-    require 'net/sftp'
-    Net::SFTP.start('host', 'username', :password => 'password') do |sftp|
-      sftp.download!("/path/to/remote", "/path/to/local")
+    remote_file_md5_sum = "d472821707a0cc2d25e884b9766ebe8f"
+    local_file_md5_sum = ""
+    
+    Net::SFTP.start('ubuntu', 'oleg', :password => 'oleg') do |sftp|
+      sftp.download!(@@remote_file, @@local_file)
     end
+    
+    local_file_md5_sum = Digest::MD5.hexdigest(File.read(@@local_file))
+    assert_equal(local_file_md5_sum, remote_file_md5_sum)
   end
 end
 
