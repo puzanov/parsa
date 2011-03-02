9fef02ac56061476d7d4dc2f6c03e856ae1d6a66
puzanov <puzanov@gmail.com>
2011-03-02 17:02:31 +0600

    working with log filename generation

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "http://rubygems.org"
 gem "net-ssh", "2.1.2"
 gem "net-sftp", "2.0.5", :require => "net/sftp"
+gem "activesupport", "3.0.5", :require => "active_support/all"
+gem "i18n", "0.5.0"
---
+++ test/log_filename_test.rb
@@ -0,0 +1,10 @@
+require "config"
+require "test/unit"
+
+class LogFilenameTest < Test::Unit::TestCase
+  def test_get
+    #filename = LogFilename.get;
+    #puts 1.day.ago
+    puts 1.month.from_now
+  end
+end
