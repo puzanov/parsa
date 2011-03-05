34074117f292ee773d077a6c220ecf977237c5a1
puzanov <puzanov@gmail.com>
2011-03-05 14:45:45 +0600

    added more methods to log_filename

--- lib/log_filename.rb
+++ lib/log_filename.rb
@@ -1,6 +1,13 @@
 class LogFilename
-  def self.get
+  def self.minus1day
     date = 1.day.ago.to_date.to_s.split("-")
     "access-#{date[0]}_#{date[1]}_#{date[2]}.log"
+  end
+  def self.current
+    date = DateTime.now.to_date.to_s.split("-")
+    "access-#{date[0]}_#{date[1]}_#{date[2]}.log"
+  end
+  def self.past date
+    "access-#{date}.log"
   end
 end
--- test/log_filename_test.rb
+++ test/log_filename_test.rb
@@ -4,8 +4,19 @@ require "lib/log_filename"
 
 class LogFilenameTest < Test::Unit::TestCase
   def test_get_minus1day
-    filename = LogFilename.get
+    filename = LogFilename.send :minus1day
     date = 1.day.ago.to_date.to_s.split("-")
     assert_equal filename, "access-#{date[0]}_#{date[1]}_#{date[2]}.log"
+  end
+
+  def test_get_actual
+    filename = LogFilename.send :current
+    date = DateTime.now.to_date.to_s.split("-")
+    assert_equal filename, "access-#{date[0]}_#{date[1]}_#{date[2]}.log"
+  end
+
+  def test_get_past 
+    filename = LogFilename.send :past, "2011_03_01"    
+    assert_equal filename, "access-2011_03_01.log"
   end
 end
