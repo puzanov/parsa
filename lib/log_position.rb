4010e574b758a67293c3fdf9ef5f77ecf76ebf4c
puzanov <puzanov@gmail.com>
2011-03-06 23:43:41 +0600

    log_position generation extracted

--- lib/log_position.rb
+++ lib/log_position.rb
@@ -1,4 +1,18 @@
 class LogPosition < ActiveRecord::Base
   set_table_name :log_position
+  def self.get_existing_or_new data
+    log_position = LogPosition.where(:filepath => data.remote_file, :host => data.host).first
+    if log_position.nil?
+      LOG.info "#{data.host} Working with new log"
+      log_position = LogPosition.new
+      log_position.host = data.host
+      log_position.filepath = data.remote_file
+      log_position.position = 0
+    else
+      LOG.warn "#{data.host} Log is not new!"
+      LOG.warn "#{data.host} skipping #{log_position.position} lines"
+    end
+    return log_position
+  end
 end
 
--- parsa.rb
+++ parsa.rb
@@ -28,18 +28,8 @@ CONFIG["servers"].each do |server|
   processed_lines = 0
   checkpoints = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 96, 97, 98, 99]
 
-  log_position = LogPosition.where(:filepath => data.remote_file, :host => data.host).first
-  if log_position.nil?
-    LOG.info "#{data.host} Working with new log"
-    log_position = LogPosition.new
-    log_position.host = data.host
-    log_position.filepath = data.remote_file
-    log_position.position = 0
-  else
-    LOG.warn "#{data.host} Log is not new!"
-    LOG.warn "#{data.host} skipping #{log_position.position} lines"
-  end
-
+  log_position = LogPosition.get_existing_or_new data
+  
   File.readlines(data.local_file).collect do |line|
     processed_lines += 1
     
@@ -49,6 +39,7 @@ CONFIG["servers"].each do |server|
 
     log_data = log_parser.parse(line)
     next if log_data.nil?
+    
     log_data_manager.save log_data
     percent = (processed_lines * 100 / lines_number)
     if checkpoints.include?(percent)
@@ -56,7 +47,7 @@ CONFIG["servers"].each do |server|
       LOG.info "#{data.host} #{percent}% is done..."
     end
   end
-  }
+  } 
 end
 
 threads.each do |thread|
